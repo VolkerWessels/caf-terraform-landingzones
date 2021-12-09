3f1c8e36e6d99d0d28ae9956a6d52be8b100172a
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2021-12-09 18:35:48 +0100

    Debug sessie source_image_id met Dimitri

--- caf_solution/vw_debug.tf
+++ caf_solution/vw_debug.tf
@@ -3,7 +3,7 @@ resource "null_resource" "debug" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-  command = "echo $VARIABLE1 >> debuglz.json; echo $VARIABLE2 >> debuglz.json; echo $VARIABLE3 >> debuglz.json; cat debuglz.json"
+  command = "echo $VARIABLE1 >> debug_lz.json; echo $VARIABLE2 >> debug_lz.json; echo $VARIABLE3 >> debug_lz.json; cat debug_lz.json"
     environment = {
       VARIABLE1 = jsonencode(data.terraform_remote_state.remote)
       VARIABLE2 = jsonencode(var.image_definitions)
