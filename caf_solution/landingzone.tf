d2926b36a69ed0d04a23a64f3362c133564f6031
Dimitri <>
2021-12-15 10:11:48 +0100

    debug data.terraform remote state

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -75,7 +75,7 @@ resource "null_resource" "debug" {
   provisioner "local-exec" {
   command = "echo $VARIABLE1 >> debuglz.json; echo $VARIABLE2 >> debuglz.json; echo $VARIABLE3 >> debuglz.json; cat debuglz.json"
     environment = {
-      VARIABLE1 = jsonencode(local.shared_services)
+      VARIABLE1 = jsonencode(data.terraform_remote_state.remote)
       VARIABLE2 = jsonencode(var.image_definitions)
       VARIABLE3 = jsonencode(local.remote)
     } 
