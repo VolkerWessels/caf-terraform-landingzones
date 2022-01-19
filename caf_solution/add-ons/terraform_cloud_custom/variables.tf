01a3e97c57ab01b1c3b2cf3addde7754ab352610
hyperfocus1337 <hyperfocus@hotmail.com>
2022-01-19 10:42:11 +0100

    Add terraform_cloud_custom add-on

---
+++ caf_solution/add-ons/terraform_cloud_custom/variables.tf
@@ -0,0 +1,27 @@
+variable "landingzone" {
+  default = {}
+}
+
+variable "tags" {
+  default = null
+}
+
+variable "tfe_organizations" {
+  default = {}
+}
+
+variable "tfe_workspaces" {
+  default = {}
+}
+
+variable "tfe_variables" {
+  default = {}
+}
+
+variable "tfe_servers" {
+  default = {}
+}
+
+variable "tfe_agents" {
+  default = {}
+}
