e0d5ee71394b3d2ea6a3e10166aef900618aa717
Marcel Heek <mheek@vwinfra.nl>
2022-03-18 15:26:11 +0100

    add additional variables for managment cpability

--- caf_solution/add-ons/caf_eslz/enterprise_scale.tf
+++ caf_solution/add-ons/caf_eslz/enterprise_scale.tf
@@ -27,6 +27,7 @@ module "enterprise_scale" {
   subscription_id_overrides      = local.subscription_id_overrides
   deploy_identity_resources      = var.deploy_identity_resources
   subscription_id_identity       = var.subscription_id_identity
+  subscription_id_management     = var.subscription_id_management
   configure_identity_resources   = var.configure_identity_resources
   deploy_management_resources    = var.deploy_management_resources
   configure_management_resources = var.configure_management_resources
