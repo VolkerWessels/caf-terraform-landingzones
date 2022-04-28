6aa6c017d5819827070d13655d832af0bc063c4f
Marcel Heek <mheek@vwinfra.nl>
2022-04-28 14:42:49 +0200

    change to azurerm provide ~> 3.0.2

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.96.0"
+      #version = "~> 2.96.0"
+      version = "~> 3.0.2"
     }
   }
   required_version = ">= 0.14"
