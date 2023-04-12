26e46c057749edfbb0bfdcfa7263659a3ac4e3aa
Berend <bvwaalwijk@volkerwessels.com>
2023-04-12 11:01:18 +0200

    rm comments. fixed

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -2,10 +2,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      # version = "~> 2.96"
+      source  = "hashicorp/azurerm"
       version = ">= 3.35.0"
-      #version = ">= 3.02" # upped the version to allow v2.0.0+ caf_eslz support
     }
   }
   required_version = ">= 1.1.0"
