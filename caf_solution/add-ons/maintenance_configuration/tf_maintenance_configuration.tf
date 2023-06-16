9c85cce1e21d50837706a0ab06d3077b8a30f137
Mathieu van Wijngaarden <mvwijngaarden@volkerwessels.com>
2023-06-16 15:41:44 +0200

    Change patch block

--- caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
+++ caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
@@ -15,19 +15,19 @@ resource "azurerm_maintenance_configuration" "mc" {
     duration             = try(each.value.window.duration, null)
   }
   install_patches {
-    scope  = try(each.value.install_patches.scope, null)
+    windows {
+      classifications_to_include = try([each.value.windows.classifications_to_include], null)
+      kb_numbers_to_exclude      = try([each.value.windows.kb_numbers_to_exclude], null)
+      kb_numbers_to_include      = try([each.value.windows.kb_numbers_to_include], null)
+    }
+    linux {
+      classifications_to_include    = try([each.value.linux.classifications_to_include])
+      package_names_mask_to_exclude = try(each.value.linux.package_names_mask_to_exclude, null)
+      package_names_mask_to_include = try(each.value.linux.package_names_mask_to_include, null)
+    }
     reboot = try(each.value.install_patches.reboot, "Never")
   }
-  windows {
-    classifications_to_include = try([each.value.windows.classifications_to_include], null)
-    kb_numbers_to_exclude      = try([each.value.windows.kb_numbers_to_exclude], null)
-    kb_numbers_to_include      = try([each.value.windows.kb_numbers_to_include], null)
-  }
-  linux {
-    classifications_to_include    = try([each.value.linux.classifications_to_include])
-    package_names_mask_to_exclude = try(each.value.linux.package_names_mask_to_exclude, null)
-    package_names_mask_to_include = try(each.value.linux.package_names_mask_to_include, null)
-  }
+
   in_guest_user_patch_mode = try(each.value.in_guest_user_patch_mode, null)
   properties               = try(each.value.properties, null)
   tags                     = try(each.value.tags, null)
