1472ba793697d7d0583119f66fef64e4b55c3f20
Mathieu van Wijngaarden <mvwijngaarden@volkerwessels.com>
2023-06-16 15:25:42 +0200

    fix

--- caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
+++ caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
@@ -4,29 +4,29 @@ resource "azurerm_maintenance_configuration" "mc" {
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
 
-  location   = try(each.value.region1)
+  location   = try(each.value.location)
   scope      = try(each.value.scope, null)
   visibility = try(each.value.visibility, null)
   window = {
-    start_date_time      = try(each.value.start_date_time, null)
-    expiration_date_time = try(each.value.expiration_date_time, null)
-    recur_every          = try(each.value.recur_every, null)
-    time_zone            = try(each.value.time_zone, "W. Europe Standard Time")
-    duration             = try(each.value.duration, null)
+    start_date_time      = try(each.value.window.start_date_time, null)
+    expiration_date_time = try(each.value.window.expiration_date_time, null)
+    recur_every          = try(each.value.window.recur_every, null)
+    time_zone            = try(each.value.window.time_zone, "W. Europe Standard Time")
+    duration             = try(each.value.window.duration, null)
   }
   install_patches = {
-    scope  = try(each.value.scope, null)
-    reboot = try(each.value.reboot, "Never")
+    scope  = try(each.value.install_patches.scope, null)
+    reboot = try(each.value.install_patches.reboot, "Never")
   }
   windows = {
-    classifications_to_include = try([each.value.classifications_to_include], null)
-    kb_numbers_to_exclude      = try([each.value.kb_numbers_to_exclude], null)
-    kb_numbers_to_include      = try([each.value.kb_numbers_to_include], null)
+    classifications_to_include = try([each.value.windows.classifications_to_include], null)
+    kb_numbers_to_exclude      = try([each.value.windows.kb_numbers_to_exclude], null)
+    kb_numbers_to_include      = try([each.value.windows.kb_numbers_to_include], null)
   }
   linux = {
-    classifications_to_include    = try([each.value.classifications_to_include])
-    package_names_mask_to_exclude = try(each.value.package_names_mask_to_exclude, null)
-    package_names_mask_to_include = try(each.value.package_names_mask_to_include, null)
+    classifications_to_include    = try([each.value.linux.classifications_to_include])
+    package_names_mask_to_exclude = try(each.value.linux.package_names_mask_to_exclude, null)
+    package_names_mask_to_include = try(each.value.linux.package_names_mask_to_include, null)
   }
   in_guest_user_patch_mode = try(each.value.in_guest_user_patch_mode, null)
   properties               = try(each.value.properties, null)
