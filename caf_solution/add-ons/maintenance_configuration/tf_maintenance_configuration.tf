6b086ded6eb95e3b2a27e924c82b2bc01448b032
Mathieu van Wijngaarden <mvwijngaarden@volkerwessels.com>
2023-06-16 15:31:50 +0200

    maak er een block fan.nl

--- caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
+++ caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
@@ -7,23 +7,23 @@ resource "azurerm_maintenance_configuration" "mc" {
   location   = try(each.value.location)
   scope      = try(each.value.scope, null)
   visibility = try(each.value.visibility, null)
-  window = {
+  window {
     start_date_time      = try(each.value.window.start_date_time, null)
     expiration_date_time = try(each.value.window.expiration_date_time, null)
     recur_every          = try(each.value.window.recur_every, null)
     time_zone            = try(each.value.window.time_zone, "W. Europe Standard Time")
     duration             = try(each.value.window.duration, null)
   }
-  install_patches = {
+  install_patches {
     scope  = try(each.value.install_patches.scope, null)
     reboot = try(each.value.install_patches.reboot, "Never")
   }
-  windows = {
+  windows {
     classifications_to_include = try([each.value.windows.classifications_to_include], null)
     kb_numbers_to_exclude      = try([each.value.windows.kb_numbers_to_exclude], null)
     kb_numbers_to_include      = try([each.value.windows.kb_numbers_to_include], null)
   }
-  linux = {
+  linux {
     classifications_to_include    = try([each.value.linux.classifications_to_include])
     package_names_mask_to_exclude = try(each.value.linux.package_names_mask_to_exclude, null)
     package_names_mask_to_include = try(each.value.linux.package_names_mask_to_include, null)
