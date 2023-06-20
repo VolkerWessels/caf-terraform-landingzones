810d84b337f39b36681cbe47563f90174aad24f1
Mathieu van Wijngaarden <mvwijngaarden@volkerwessels.com>
2023-06-20 08:29:19 +0200

    Remove [] from kb lists

--- caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
+++ caf_solution/add-ons/maintenance_configuration/tf_maintenance_configuration.tf
@@ -5,7 +5,7 @@ resource "azurerm_maintenance_configuration" "mc" {
   resource_group_name = each.value.resource_group_name
 
   location   = try(each.value.location)
-  scope      = try(each.value.scope, null)
+  scope      = try(each.value.scope)
   visibility = try(each.value.visibility, null)
   window {
     start_date_time      = try(each.value.window.start_date_time, null)
@@ -16,14 +16,14 @@ resource "azurerm_maintenance_configuration" "mc" {
   }
   install_patches {
     windows {
-      classifications_to_include = try(each.value.install_patches.windows.classifications_to_include, [])
-      kb_numbers_to_exclude      = try(each.value.install_patches.windows.kb_numbers_to_exclude, [])
-      kb_numbers_to_include      = try(each.value.install_patches.windows.kb_numbers_to_include, [])
+      classifications_to_include = try(each.value.install_patches.windows.classifications_to_include)
+      kb_numbers_to_exclude      = try(each.value.install_patches.windows.kb_numbers_to_exclude)
+      kb_numbers_to_include      = try(each.value.install_patches.windows.kb_numbers_to_include)
     }
     linux {
-      classifications_to_include    = try(each.value.install_patches.linux.classifications_to_include, [])
-      package_names_mask_to_exclude = try(each.value.install_patches.linux.package_names_mask_to_exclude, [])
-      package_names_mask_to_include = try(each.value.install_patches.linux.package_names_mask_to_include, [])
+      classifications_to_include    = try(each.value.install_patches.linux.classifications_to_include)
+      package_names_mask_to_exclude = try(each.value.install_patches.linux.package_names_mask_to_exclude)
+      package_names_mask_to_include = try(each.value.install_patches.linux.package_names_mask_to_include)
     }
     reboot = try(each.value.install_patches.reboot, null)
   }
