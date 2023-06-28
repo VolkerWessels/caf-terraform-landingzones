1abc052e7cf7e56e6efea9cf957c64553e42f3af
BerendvW <91737467+BerendvW@users.noreply.github.com>
2023-06-28 15:29:31 +0200

    private dns resolver variabelen toegevoegd

--- caf_solution/local.networking.tf
+++ caf_solution/local.networking.tf
@@ -47,6 +47,12 @@ locals {
       private_dns                                             = var.private_dns
       private_dns_vnet_links                                  = var.private_dns_vnet_links
       private_endpoints                                       = var.private_endpoints
+      private_dns_resolvers                                   = var.private_dns_resolvers
+      private_dns_resolver_inbound_endpoints                  = var.private_dns_resolver_inbound_endpoints
+      private_dns_resolver_outbound_endpoints                 = var.private_dns_resolver_outbound_endpoints
+      private_dns_resolver_forwarding_rules                   = var.private_dns_resolver_forwarding_rules
+      private_dns_resolver_dns_forwarding_rulesets            = var.private_dns_resolver_dns_forwarding_rulesets
+      private_dns_resolver_virtual_network_links              = var.private_dns_resolver_virtual_network_links
       public_ip_addresses                                     = var.public_ip_addresses
       public_ip_prefixes                                      = var.public_ip_prefixes
       relay_hybrid_connection                                 = var.relay_hybrid_connection
--- caf_solution/variables.networking.tf
+++ caf_solution/variables.networking.tf
@@ -111,6 +111,24 @@ variable "private_dns" {
 variable "private_dns_vnet_links" {
   default = {}
 }
+variable "private_dns_resolvers" {
+  default = {}
+}
+variable "private_dns_resolver_inbound_endpoints" {
+  default = {}
+}
+variable "private_dns_resolver_outbound_endpoints" {
+  default = {}
+}
+variable "private_dns_resolver_forwarding_rules" {
+  default = {}
+}
+variable "private_dns_resolver_dns_forwarding_rulesets" {
+  default = {}
+}
+variable "private_dns_resolver_virtual_network_links" {
+  default = {}
+}
 variable "public_ip_addresses" {
   default = {}
 }
