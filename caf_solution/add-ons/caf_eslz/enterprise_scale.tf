92f7757a42c6989447febf7383651d8a69984565
IdifixNL <nbaburek@volkerwessels.com>
2023-08-09 16:16:15 +0200

    source issues

--- caf_solution/add-ons/caf_eslz/enterprise_scale.tf
+++ caf_solution/add-ons/caf_eslz/enterprise_scale.tf
@@ -4,7 +4,9 @@ module "enterprise_scale" {
   #source  = "Azure/caf-enterprise-scale/azurerm"
   #source = "git::https://github.com/VolkerWessels/caf-enterprise-scale.git//azurerm"
 #  source = "git@github.com:VolkerWessels/caf-enterprise-scale.git//azurerm"
-  source = "github.com/VolkerWessels/terraform-azurerm-caf-enterprise-scale/azurerm"
+  #source = "github.com/VolkerWessels/terraform-azurerm-caf-enterprise-scale/azurerm"
+  source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf-enterprise-scale.git?ref=3.3.0//azurerm"
+
   version = "3.3.0"
   #version = "4.2.0"
 
