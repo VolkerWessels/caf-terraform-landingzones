dc75556c6d42038a0b601cd68e1893a3feaaee96
IdifixNL <nbaburek@volkerwessels.com>
2023-08-09 15:23:15 +0200

    change module enterprise_scale source to Volkerwessels

--- caf_solution/add-ons/caf_eslz/enterprise_scale.tf
+++ caf_solution/add-ons/caf_eslz/enterprise_scale.tf
@@ -3,7 +3,8 @@
 module "enterprise_scale" {
   #source  = "Azure/caf-enterprise-scale/azurerm"
   #source = "git::https://github.com/VolkerWessels/caf-enterprise-scale.git//azurerm"
-  source = "git@github.com:VolkerWessels/caf-enterprise-scale.git//azurerm"
+#  source = "git@github.com:VolkerWessels/caf-enterprise-scale.git//azurerm"
+  source = "git@github.com:VolkerWessels/terraform-azurerm-caf-enterprise-scale.git//azurerm"
   #version = "3.3.0"
   #version = "4.2.0"
 
