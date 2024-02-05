0e4225a88d521b00e4641ed07387423c7e752f6b
Marcel Heek <mheek@volkerwessels.com>
2024-02-05 17:38:42 +0100

    Update azurerm provider versions and add additional providers

--- caf_solution/add-ons/caf_eslz/main.tf
+++ caf_solution/add-ons/caf_eslz/main.tf
@@ -7,7 +7,7 @@ terraform {
       version = ">= 3.54" # upped the version to allow v4.0.1+ caf_eslz support
     }
   }
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3.5"
   # experiments      = [module_variable_optional_attrs]
 }
 
@@ -17,5 +17,30 @@ provider "azurerm" {
   # partner identifier for CAF Terraform landing zones.
   features {}
 }
+
+provider "azurerm" {
+  partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
+  alias      = "connectivity"
+  features {}
+  subscription_id = local.subscription_id_connectivity
+  tenant_id       = var.tenant_id
+}
+
+provider "azurerm" {
+  partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
+  alias      = "management"
+  features {}
+  subscription_id = var.subscription_id_management == null ? data.azurerm_client_config.current.subscription_id : var.subscription_id_management
+  tenant_id       = var.tenant_id
+}
+
+provider "azurerm" {
+  partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
+  alias      = "identity"
+  features {}
+  subscription_id = var.subscription_id_identity == null ? data.azurerm_client_config.current.subscription_id : var.subscription_id_identity
+  tenant_id       = var.tenant_id
+}
+
 
 data "azurerm_client_config" "current" {}
